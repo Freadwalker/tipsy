a6128c341d1f5d36ee4110c6b31276a4d15773eb
Frederic Bierbaum <frederic.bierbaum@outlook.com>
2019-10-27 21:22:10 +0100

    Finished some stuff lel

--- server/models/questions.js
+++ server/models/questions.js
@@ -12,7 +12,23 @@ let questions=[
 "The class trip field was completely ruined by_______",
 "I got 99 problems but _____ ain't one",
 "But before I kill you Mr.Bond I must show you_____",
-"After the earthquake Sean Penn brought_______to the people in Haiti."
+"After the earthquake Sean Penn brought_______to the people in Haiti.",
+"During Sex I like to think about _______",
+"In the new Disney Channel Movie, Hannah Montana struggles with _______",
+"A romantic candlelight dinner would be incomplete without _______",
+"Next from J.K. Harry Potter and the Chamber of _______",
+"Instead of coal, Santa now gives the bad children ________",
+"What's worse than the Holocaust?",
+"I always carry_______ in my purse.",
+"Sometimes I just look at the kids and I think______",
+"What's your favorite kind of torture?",
+"10% of adults admit to having an addiction to _______",
+"I'm sorry professor, but I couldn't complete my homework because of _______",
+"The last thing you see before you die",
+"What's the next Happy Meal Toy?",
+"What gives me uncontrollable Gas?",
+"TSA guidelines now prohibit ______ on airplanes."
+
 ]   
 
 const questionSchema= new Schema({
